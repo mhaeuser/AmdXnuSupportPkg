7c19ce2ef54e4ca8cffa91c8fb8fda1aa7ddd0bd
Download-Fritz <download-fritz@outlook.com>
2018-11-11 07:41:25 +0100

    Merge Helpers into MsrIntercept.c.

--- AmdIntelEmuDxe/Intercepts/Msr/MsrIntercept.c
+++ AmdIntelEmuDxe/Intercepts/Msr/MsrIntercept.c
@@ -3,6 +3,7 @@
 #include <Register/ArchitecturalMsr.h>
 
 #include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
 
 #include "../../AmdIntelEmu.h"
 
@@ -38,4 +39,35 @@ AmdEmuInterceptWrmsr (
       break;
     }
   }
+}
+
+UINT64
+AmdIntelEmuInternalReadMsrValue64 (
+  IN OUT UINT64             *Rax,
+  IN OUT AMD_EMU_REGISTERS  *Registers
+  )
+{
+  UINT64 Value;
+
+  ASSERT (Rax != NULL);
+  ASSERT (Registers != NULL);
+
+  Value  = *Rax;
+  Value |= (Registers->Rdx >> 31U);
+
+  return Value;
+}
+
+VOID
+AmdIntelEmuInternalWriteMsrValue64 (
+  IN OUT UINT64             *Rax,
+  IN OUT AMD_EMU_REGISTERS  *Registers,
+  IN     UINT64             Value
+  )
+{
+  ASSERT (Rax != NULL);
+  ASSERT (Registers != NULL);
+
+  *Rax           = (Value & 0xFFFFFFFFU);
+  Registers->Rdx = (Value << 31U);
 }
