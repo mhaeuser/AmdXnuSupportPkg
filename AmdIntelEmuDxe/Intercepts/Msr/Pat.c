57bd86c6d53647bd3227dff0ee6eeab4613909b3
Download-Fritz <download-fritz@outlook.com>
2018-11-12 07:18:16 +0100

    Use bitfield to alter the PAT register.

--- AmdIntelEmuDxe/Intercepts/Msr/Pat.c
+++ AmdIntelEmuDxe/Intercepts/Msr/Pat.c
@@ -7,13 +7,7 @@
 
 #include "../../AmdIntelEmu.h"
 
-#define PATENTRY(Value, Type) (((Type) & 0x0FULL) << ((Value) * 8ULL))
-#define PAT_UC       0x00ULL
-#define PAT_WC       0x01ULL
-#define PAT_WT       0x04ULL
-#define PAT_WP       0x05ULL
-#define PAT_WB       0x06ULL
-#define PAT_UCMINUS  0x07ULL
+#define PAT_WC  0x01U
 
 VOID
 AmdIntelEmuInternalWrmsrPat (
@@ -21,7 +15,7 @@ AmdIntelEmuInternalWrmsrPat (
   IN OUT AMD_EMU_REGISTERS  *Registers
   )
 {
-  UINT64 Value;
+  MSR_IA32_PAT_REGISTER PatMsr;
 
   ASSERT (Rax != NULL);
   ASSERT (Registers != NULL);
@@ -31,8 +25,8 @@ AmdIntelEmuInternalWrmsrPat (
   // TODO: Is forcing the other PAT entries into default as done in the kenrel
   //       necessary? Why does this work? Do all WTs need to be forced into WC?
   //
-  Value  = AmdIntelEmuInternalReadMsrValue64 (Rax, Registers);
-  Value &= ~(UINT64)(PATENTRY (1U, 0x0FU) | PATENTRY (5U, 0x0FU));
-  Value |= (PATENTRY (1U, PAT_WC) | PATENTRY (5U, PAT_WC));
-  AsmWriteMsr64 (MSR_IA32_PAT, Value);
+  PatMsr.Uint64   = AmdIntelEmuInternalReadMsrValue64 (Rax, Registers);
+  PatMsr.Bits.PA1 = PAT_WC;
+  PatMsr.Bits.PA5 = PAT_WC;
+  AsmWriteMsr64 (MSR_IA32_PAT, PatMsr.Uint64);
 }
