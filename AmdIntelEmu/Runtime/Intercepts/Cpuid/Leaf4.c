42d861eca69ad08b458b9b6946212b27b8fe7dff
Download-Fritz <download-fritz@outlook.com>
2018-12-19 23:10:13 +0100

    Correctly return the core count in CPUID Leaf 4.

--- AmdIntelEmu/Runtime/Intercepts/Cpuid/Leaf4.c
+++ AmdIntelEmu/Runtime/Intercepts/Cpuid/Leaf4.c
@@ -3,6 +3,8 @@
 #include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
 
+#include "../../../AmdIntelEmu.h"
+
 VOID
 AmdIntelEmuInternalCpuidLeaf4 (
   IN  UINT32  SubIndex,
@@ -12,12 +14,38 @@ AmdIntelEmuInternalCpuidLeaf4 (
   OUT UINT32  *Edx
   )
 {
+  CPUID_AMD_APIC_ID_SIZE_CORE_COUNT_ECX  CoreCountEcx;
+  CPUID_AMD_COMPUTE_UNIT_IDENTIFIERS_EBX CuIdEbx;
+  UINT16                                 ThreadCount;
+  UINT16                                 CoreCount;
+
   ASSERT (Eax != NULL);
   ASSERT (Ebx != NULL);
   ASSERT (Ecx != NULL);
   ASSERT (Edx != NULL);
   //
-  // Intel Leaf 4 and AMD Leaf 0x8000001D are compatible.
+  // TODO: Abstract for shared usage with the MSR.
+  //
+  AsmCpuid (
+    CPUID_AMD_APIC_ID_SIZE_CORE_COUNT,
+    NULL,
+    NULL,
+    &CoreCountEcx.Uint32,
+    NULL
+    );
+  ThreadCount = (UINT16)(CoreCountEcx.Bits.NC + 1);
+
+  AsmCpuid (
+    CPUID_AMD_COMPUTE_UNIT_IDENTIFIERS,
+    NULL,
+    &CuIdEbx.Uint32,
+    NULL,
+    NULL
+    );
+  CoreCount = (ThreadCount / (UINT16)(CuIdEbx.Bits.CoresPerComputeUnit + 1));
+  //
+  // Intel Leaf 4 and AMD Leaf 0x8000001D are mostly compatible.
   //
   AsmCpuidEx (0x8000001D, SubIndex, Eax, Ebx, Ecx, Edx);
+  *Eax = BitFieldWrite32 (*Eax, 26, 31, (CoreCount - 1));
 }
